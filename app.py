c0139e62d8d6d0edd536a54b147fbde2602b080a
rahul985s <150263965+rahul985s@users.noreply.github.com>
2025-11-04 20:19:14 +0530

    Fix token and host error for Render deployment

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ with open("knowledge_base.json", "r") as f:
     KNOWLEDGE_BASE = json.load(f)
 
 # --- Telegram setup ---
-TELEGRAM_TOKEN = os.getenv("7441762426:AAG6CTvPAFFV-dRbtjXSNT6OLAKcbjwPzGU")
+TELEGRAM_TOKEN = "7441762426:AAG6CTvPAFFV-dRbtjXSNT6OLAKcbjwPzGU"
 TELEGRAM_URL = "https://api.telegram.org/bot{7441762426:AAG6CTvPAFFV-dRbtjXSNT6OLAKcbjwPzGU}"
 
 # --- Function to handle queries ---
@@ -38,4 +38,4 @@ def chat():
     return jsonify({"reply": handle_query(msg)})
 
 if __name__== "__main__":
-    app.run( host="0.0.0.0.",port=5000)
+    app.run( host="0.0.0.0",port=5000)
